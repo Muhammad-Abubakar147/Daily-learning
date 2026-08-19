f6fbe469030dc97b1db974096fb8e5aacef37a35
Muhammad Abubakar <brandmirza702@gmail.com>
2026-08-19 23:03:26 +0500

    New Turtle

--- new.py
+++ new.py
@@ -1,13 +1,26 @@
-from turtle import * 
+# from turtle import * 
+# speed(0)
+# bgcolor("black")
+# color("aqua")
+# for i in range (160):
+#     rt(i)
+#     circle(150,i)
+#     fd(i)
+#     rt(90)
+    
+# done()
+    
+    
+#Second 
+
+from turtle import *
+
 speed(0)
 bgcolor("black")
 color("aqua")
-for i in range (160):
-    rt(i)
-    circle(150,i)
-    fd(i)
-    rt(90)
-    
+
+for i in range(36):
+    circle(100)
+    rt(10)
+
 done()
-    
-    
